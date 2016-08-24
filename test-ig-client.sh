a28c3fb4e673b1b2dd1f28ad0dd26c3ff4eeb0c6
Smith Kennedy <smith.kennedy@hp.com>
2016-08-24 10:31:58 -0700

    first placeholder scripts

---
+++ test-ig-client.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# This script serves as a framework for evaluating an IPP Client for
+# the qualities of its implementation of the areas covered by the
+# IPP Implementor's Guide v2.
+#
+# This script uses "ippserver" as the core of its testing system.
+#
+
+echo "Usage: "`basename $0`" resourcepath [port]"
+
